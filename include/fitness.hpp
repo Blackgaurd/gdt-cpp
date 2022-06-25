cc18b5364b2c13fd8c74d15a46f336e2046736eb
bryan <bryan.deng002@gmail.com>
2022-06-24 22:59:26 -0400

    update fitness function declarations

--- include/fitness.hpp
+++ include/fitness.hpp
@@ -11,26 +11,16 @@ float tanh(int depth, int optimal_depth);
 float sigmoid(int depth, int optimal_depth);
 float negative_quadratic(int depth, int optimal_depth);
 
-struct _2D_Array_i {
-    int first, second;
-};
-
 class FitnessEvaluator {
-   private:
     // no idea what a1 and a2 are :)
     float a1, a2;
-    // float arrays
-    float *x, *y;
-    // float array size value
-    int x_size, y_size;
     // funciton
     std::function<float(int, int)> f2_func;
 
-   public:
     FitnessEvaluator(float a1, float a2, std::function<float(int, int)> f2_func);
     ~FitnessEvaluator();
 
     void _init();
-    float accuracy(DecisionTree);
-    float call(DecisionTree);
+    float accuracy(const DecisionTree &individual);
+    float operator() (const DecisionTree &individual);
 };
